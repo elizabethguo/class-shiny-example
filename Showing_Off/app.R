fc80badc8fd273a0db62cad0bd35f1a468dae6cf
Elizabeth Guo <elizabethguo@college.harvard.edu>
2019-10-22 12:29:08 -0400

    Commit.

--- Showing_Off/app.R
+++ Showing_Off/app.R
@@ -21,7 +21,7 @@ ui <- fluidPage(
             sliderInput("bins",
                         "Number of bins:",
                         min = 1,
-                        max = 100,
+                        max = 50,
                         value = 30)
         ),
 
